d5c67a00a60148e5a8d64843f1e92676970be391
ttalova <talova0304@gmail.com>
2022-03-26 02:45:15 +0300

    commit

--- src/array_stack.cpp
+++ src/array_stack.cpp
@@ -12,48 +12,74 @@ namespace assignment {
       throw std::invalid_argument("capacity is not positive");
     }
 
-    // Write your code here ...
+      capacity_ = capacity;
+      data_ = new int[capacity_];
+      std::fill(&data_[0], &data_[capacity_], 0);
   }
 
   ArrayStack::~ArrayStack() {
-    // Write your code here ...
+    size_ = 0;
+    capacity_ = 0;
+    delete[] data_;
+    data_ = nullptr;
   }
 
   void ArrayStack::Push(int value) {
-    // Write your code here ...
+      if (size_ == capacity_) {
+          Resize(capacity_ + kCapacityGrowthCoefficient);
+      }
+      data_[size_] = value;
+      size_ ++;
   }
 
   bool ArrayStack::Pop() {
-    // Write your code here ...
+      if (size_ > 0){
+          data_[size_ - 1] = 0;
+          size_--;
+          return true;
+      }
     return false;
   }
 
   void ArrayStack::Clear() {
-    // Write your code here ...
+      std::fill(&data_[0], &data_[capacity_], 0);
+      size_ = 0;
   }
 
   std::optional<int> ArrayStack::Peek() const {
-    // Write your code here ...
+    if (size_ > 0){
+        return data_[size_ - 1];
+    }
     return std::nullopt;
   }
 
   bool ArrayStack::IsEmpty() const {
     // Write your code here ...
-    return false;
+    return size_== 0;
   }
 
   int ArrayStack::size() const {
     // Write your code here ...
-    return 0;
+    return size_;
   }
 
   int ArrayStack::capacity() const {
     // Write your code here ...
-    return 0;
+    return capacity_;
   }
 
   bool ArrayStack::Resize(int new_capacity) {
-    // Write your code here ...
+      if (new_capacity > 0 and new_capacity > capacity_) {
+          capacity_ = new_capacity;
+          int *data2_ = new int[capacity_];
+          std::fill(&data2_[0], &data2_[capacity_], 0);
+          for (int i = 0; i < capacity_; i++) {
+              data2_[i] = data_[i];
+          }
+          delete[] data_;
+          data_ = data2_;
+          return true;
+      }
     return false;
   }
 
